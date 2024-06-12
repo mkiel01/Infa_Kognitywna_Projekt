6777b877b887cb5264e4d51dfe88262179ec9c52
proman3419 <proman7268@gmail.com>
2024-06-12 10:48:39 +0200

    Renamed

--- bone_fracture_detection/data2/rename_files.sh
+++ bone_fracture_detection/data2/rename_files.sh
@@ -3,11 +3,13 @@
 process_data_batch() {
     for category in $1/*; do
         cd "$category"
-        i=0
+        i=10000
         for file in ./*; do
-            echo "Moving $file to $i.jpg"
-            mv "$file" "$i.jpg"
-            i=$((i+1))
+            if [[ $file != "./*" ]]; then
+                echo "Moving $file to $i.jpg"
+                mv "$file" "$i.jpg"
+                i=$((i+1))
+            fi
         done
         cd ../..
     done
