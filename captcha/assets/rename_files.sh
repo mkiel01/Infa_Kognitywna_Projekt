3549497b189d7a7319de4bb6b077c2da10ff9ef7
Michał Kiełkowski <mkielkowski01@gmail.com>
2024-06-17 00:39:07 +0200

    update

---
+++ captcha/assets/rename_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+process_data_batch() {
+    for category in $1/*; do
+        cd "$category"
+        i=0
+        for file in ./*; do
+            if [[ $file != "./*" ]]; then
+                echo "Moving $file to $i.jpg"
+                # mv "$file" "$i.jpg"
+                i=$((i+1))
+            fi
+        done
+        cd ../..
+    done
+}
+
+process_data_batch level1
+process_data_batch level2
+process_data_batch level3
